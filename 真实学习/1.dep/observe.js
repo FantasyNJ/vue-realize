3e63647c3fff5cf93c1007b09467d1557aabac27
宁杰 <ningjie@qijiayoudao.com>
2018-03-02 18:33:40 +0800

    defineReactive

--- 真实学习/1.dep/observe.js
+++ 真实学习/1.dep/observe.js
@@ -35,8 +35,34 @@ function observe(val) {
 }
 
 //
-function defineReactive() {
+function defineReactive(obj, key, val) {
 
+
+    // 检查defineProperty的属性值
+    const property = Object.getOwnPropertyDescriptor(obj, key)
+    // 如果属性值的configurable为不可设置状态则阻止执行防止报错
+    if (property && property.configurable === false) {
+        return
+    }
+
+    // 声明getter和setter
+    const getter = property && property.get;
+    const setter = property && property.set;
+
+    Object.defineProperty(obj, key, {
+        enumerable: true,
+        configurable: true,   // alpha版本是false，正式版变为true了，不知是处于什么考虑？？？？
+        get(){
+            const value = getter ? getter.call(obj) : val;
+            return val;
+        },
+        set(newVal){
+            // 指没有发生变化时不触发
+            if(val === newVal){
+                return;
+            }
+        }
+    })
 }
 
 export default observe;
